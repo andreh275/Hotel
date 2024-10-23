510ad6175baf7cf8be099dd1106e140afaed5b6b
andreh275 <andreh20418@gmail.com>
2024-10-23 11:20:04 -0500

    main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@ import App from './App.vue'
 import { router } from "./routes/routes.js"
 
 // Importaciones de Quasar
-import { Quasar } from 'quasar'
-import 'quasar/dist/quasar.css'
-import iconSet from 'quasar/icon-set/material-icons'
+import { Quasar } from 'quasar';
+import quasarIconSet from 'quasar/icon-set/material-icons'; // Importa el set de íconos
+import 'quasar/dist/quasar.css'; // Estilos de Quasar
+import '@quasar/extras/material-icons/material-icons.css';
+
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+// Importación de solo las funciones necesarias
+
+
+
+
 
 // Crear la aplicación de Vue
 const app = createApp(App)
@@ -13,7 +22,7 @@ const app = createApp(App)
 // Configurar Quasar en la aplicación
 app.use(Quasar, {
   plugins: {}, // Si utilizas plugins de Quasar, agrégalos aquí
-  iconSet: iconSet, // Establece el conjunto de iconos a usar (material-icons)
+  iconSet: quasarIconSet, // Habilita Material Icons
 })
 
 // Configurar el enrutador
