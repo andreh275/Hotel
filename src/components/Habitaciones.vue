7b201d8ac23e706eac58c7f69c40454d7f0b82ab
andreh275 <andreh20418@gmail.com>
2024-10-28 09:34:41 -0500

    Habitaciones.vue

--- src/components/Habitaciones.vue
+++ src/components/Habitaciones.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="hotel-page">
-
     <!-- Banner con múltiples imágenes -->
-    <div class="banner"
-         @mouseenter="onMouseEnter"
-         @mouseleave="onMouseLeave">
+    <div class="banner" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
       <button @click="prevSlide" class="arrow arrow-left">←</button>
       <div class="banner-content">
         <div class="banner-image-wrapper">
@@ -20,98 +17,193 @@
       <button @click="nextSlide" class="arrow arrow-right">→</button>
     </div>
 
-    <!-- Sección de Tarjetas de Habitaciones -->
+    <!-- Sección de Tarjetas -->
     <div class="cards-container">
-      <div v-for="(room, index) in rooms" :key="index" class="card">
-        <img :src="room.image" :alt="room.title" class="card-image" />
-        <h2 class="card-title">{{ room.title }}</h2>
-        <p class="card-description">{{ room.details }}</p>
-        <p class="card-price">{{ room.price }}</p>
-        <button class="reservation-button">Reservar</button>
+      <div v-for="(card, index) in cards" :key="index" class="card">
+        <img :src="card.image" :alt="card.title" class="card-image" />
+        <h2 class="card-title">{{ card.title }}</h2>
+        <p class="card-description">{{ card.description }}</p>
+        <p class="card-price">{{ card.price }}</p>
       </div>
     </div>
+    
+    <footer class="footer">
+      <div class="footer-content">
+        <div class="contact-info">
+          <p>Email: contacto@hotel.com</p>
+          <p>Teléfono: +57 1 2345678</p>
+          <p>
+            Dirección: Calle Falsa 123, San Gil 
+            <a href="https://www.google.com/maps/place/Calle+Falsa+123,+San+Gil,+Santander/@6.5714846,-73.1341889,15z/data=!4m6!3m5!1s0x8e4a1cb34ebf89f7:0xa3ab7d2b490a2b94!8m2!3d6.5714846!4d-73.1341889!16s%2Fg%2F11gq_pnxtk?entry=ttu" target="_blank">
+              <img src="https://img.icons8.com/ios-filled/50/ffffff/map-marker.png" alt="Mapa" />
+            </a>
+          </p>
+        </div>
+        <div class="social-media">
+          <a href="https://www.facebook.com" target="_blank">
+            <img src="https://img.icons8.com/ios-filled/50/ffffff/facebook-new.png" alt="Facebook" />
+          </a>
+          <a href="https://www.instagram.com" target="_blank">
+            <img src="https://img.icons8.com/ios-filled/50/ffffff/instagram-new.png" alt="Instagram" />
+          </a>
+          <a href="https://www.twitter.com" target="_blank">
+            <img src="https://img.icons8.com/ios-filled/50/ffffff/twitter-squared.png" alt="Twitter" />
+          </a>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
+import { ref, computed, onMounted, onBeforeUnmount } from 'vue';
+
 export default {
-  data() {
-    return {
-      currentBannerIndex: 0,
-      isHovered: false,
-      autoSlideInterval: null,
-      banners: [
-        {
-          title: "Suite Panorámica",
-          description: "Disfruta de vistas espectaculares desde esta habitación exclusiva.",
-          image: "https://webbox.imgix.net/images/xnlqhdilkviknnie/5d2c590f-277b-4285-a76c-688c4ef4a167.jpg?auto=format,compress&fit=crop&crop=entropy"
-        },
-        {
-          title: "Habitación Loft",
-          description: "Espacio moderno y acogedor para tu estancia.",
-          image: "https://hips.hearstapps.com/hmg-prod/images/casa-nildo-jose-loft-ninho-casacor-sao-paulo-2018-01-1538476788.jpg"
-        },
-        {
-          title: "Habitación con Terraza",
-          description: "Relájate en la terraza privada con vistas a las montañas.",
-          image: "https://www.hotelriberadetriana.com/wp-content/uploads/2021/05/DSC_2693-scaled.jpg"
-        },
-      ],
-      rooms: [
-        { title: "Habitación Estándar", details: "Perfecta para viajeros solos.", price: "$150 por noche", image: "https://hotelesdann.com/dann-carlton-bogota/wp-content/uploads/sites/3/2020/08/habitacin-estndar-hotel-dann-carlton-bogot-colombia_37577235816_o.jpg" },
-        { title: "Habitación Doble", details: "Ideal para parejas.", price: "$200 por noche", image: "https://www.hotelvictoriamerida.com/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/1/_/1.jpg" },
-        { title: "Habitación Familiar", details: "Espacio amplio para familias.", price: "$350 por noche", image: "https://www.swanscayhotel.com/wp-content/uploads/2022/05/hotel-en-bocas-del-toro-habitaciones-familiares.jpg" },
-        { title: "Habitación Deluxe", details: "Lujo y confort garantizados.", price: "$300 por noche", image: "https://images.mirai.com/INFOROOMS/100161192/IpZXOjQCx1VgqqWrqJ43/IpZXOjQCx1VgqqWrqJ43_large.jpg" },
-        { title: "Suite Presidencial", details: "Vive la experiencia de lujo total.", price: "$500 por noche", image: "https://s3.amazonaws.com/static-webstudio-accorhotels-usa-1.wp-ha.fastbooking.com/wp-content/uploads/sites/19/2022/03/11175445/DUF_7063-v-ok-1170x780.jpg" },
-        { title: "Habitación Ejecutiva", details: "Para viajes de negocios.", price: "$250 por noche", image: "https://www.windsortower.com/wp-content/uploads/2015/04/1-1.jpg" },
-        { title: "Habitación con Balcón", details: "Vista privilegiada a las montañas.", price: "$275 por noche", image: "https://images.homify.com/v1441997275/p/photo/image/899911/15._Habitaci%C3%B3n_2.jpg" },
-        { title: "Habitación con Jacuzzi", details: "Relájate con estilo.", price: "$400 por noche", image: "https://hotelboraboramelgar.com/wp-content/uploads/2020/01/IMG_1900.png" },
-        { title: "Habitación Pet-Friendly", details: "Espacio acogedor para ti y tu mascota.", price: "$230 por noche", image: "https://content.arquitecturaydiseno.es/medio/2020/09/04/habitacion-del-hotel-vividora-en-barcelona-pet-friendly_74dcb10f_1500x1125.jpg" },
-        { title: "Habitación Romántica", details: "Para momentos inolvidables.", price: "$350 por noche", image: "https://www.hotelpalmerasdeluz.com/media/35459/hotel-palmeras-de-luz-moniquira-noche-romantica-2.png?width=687&height=393&mode=crop" },
-        { title: "Loft Moderno", details: "Estilo urbano y minimalista.", price: "$380 por noche", image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/538278977.jpg?k=f1e90894de0803f986214c62513784bbcb4856d9cf53d731fbf51477cf37ee8a&o=&hp=1" },
-        { title: "Habitación Eco-Friendly", details: "En armonía con la naturaleza.", price: "$210 por noche", image: "https://cf.bstatic.com/xdata/images/hotel/max200/409814139.jpg?k=2587c0aa9a8b46e616970f3b042fd3b8f89002c7424bc39c67a3dcbfcb8bb4f9&o=&hp=1" }
-      ]
+  setup() {
+    const currentBannerIndex = ref(0);
+    const isHovered = ref(false);
+    const autoSlideInterval = ref(null);
+    const banners = ref([
+      {
+        title: "Disfruta de las Mejores Vistas",
+        description: "Explora nuestros paisajes",
+        image: "https://media2.utp.edu.co/programas/106/Pereira-caf.jpeg"
+      },
+      {
+        title: "Aventura Extrema",
+        description: "Vive la adrenalina con nuestros deportes extremos.",
+        image: "https://taytamaki.com/wp-content/uploads/2022/12/PUENTE-COLGANTE-1024x683.jpg"
+      },
+      {
+        title: "Relájate en el Spa",
+        description: "Descansa con nuestro servicio de spa de primera clase.",
+        image: "https://media.cylex.com.co/companies/1113/9062/images/-1269285251-img4_159204_large.jpg"
+      },
+    ]);
+    
+    const cards = ref([
+      {
+        title: "Rafting en el Río Fonce",
+        description: "Surca las aguas rápidas del Río Fonce en San Gil.",
+        price: "$150 por persona",
+        image: "https://static.roadtrip.travel/media/roadtrips/rafting-en-el-rio-fonce-desde-san-gil-santander-1200-81e6563.jpg"
+      },
+      {
+        title: "Parapente en Chicamocha",
+        description: "Vuela sobre el imponente Cañón del Chicamocha.",
+        price: "$200 por persona",
+        image: "https://www.riosycanoas.com/wp-content/uploads/2020/09/WhatsApp-Image-2020-09-26-at-17.22.48.jpeg"
+      },
+      {
+        title: "Bungee Jumping en Santander",
+        description: "Siente la adrenalina saltando al vacío desde 70 metros.",
+        price: "$180 por persona",
+        image: "https://travelerhostel.com/wp-content/uploads/elementor/thumbs/4-copia-1-qrkh3arnl178u1dl9ulcfqsm5ac66mj61u9pzl23v4.jpg"
+      },
+      {
+        title: "Ciclismo de Montaña en Boyacá",
+        description: "Recorre senderos desafiantes entre montañas y valles.",
+        price: "$80 por persona",
+        image: "https://img1.wsimg.com/isteam/ip/8d6c24da-4875-44c5-a47b-0068c8284fef/Cocuy%202008%20(188).jpg"
+      },
+      {
+        title: "Surf en Nuquí",
+        description: "Disfruta de las olas del Pacífico colombiano.",
+        price: "$250 por persona",
+        image: "https://colombiavisible.com/wp-content/uploads/2022/02/Mendihuaca-1024x576.jpg"
+      },
+      {
+        title: "Escalada en Suesca",
+        description: "Escala las famosas rocas de Suesca.",
+        price: "$100 por persona",
+        image: "https://suescalada.com/wp-content/uploads/2023/07/Escalada-en-roca-rock-climbing-Suesca_1.jpg"
+      },
+      {
+        title: "Kitesurf en La Guajira",
+        description: "Aprovecha los fuertes vientos en Cabo de la Vela.",
+        price: "$300 por persona",
+        image: "https://www.salyroca.es/media/salyroca/images/2022/10/30/2022103003172728089.jpg"
+      },
+      {
+        title: "Rappel en Cañones de Antioquia",
+        description: "Desciende cañones rodeados de naturaleza.",
+        price: "$120 por persona",
+        image: "https://s2.wklcdn.com/image_11/339507/34372923/22182008.400x300.jpg"
+      },
+      {
+        title: "Puenting en San Gil",
+        description: "Experimenta la emoción del puenting en un entorno natural.",
+        price: "$170 por persona",
+        image: "https://i.ytimg.com/vi/OpLJkBwx9Ns/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDb9MFo0DqjPJkLFQo3Al48Gmfzlg"
+      },
+      {
+        title: "Canopy en Quindío",
+        description: "Deslízate entre las copas de los árboles del Eje Cafetero.",
+        price: "$90 por persona",
+        image: "https://lh3.googleusercontent.com/proxy/eWrvpaFCfsqeaRjKq2ISI-lLSiKraz0-CPPnmo0pJXg-V63IiHV3t0ZMCyLyj6Lch78Rmad8mGsguMAdYhC0a61og1u3F7Fkz0ckzF7pnGnfMftA9swCMwXe-9MTxSU7IiYujgPS3HpV7BvrkaCwEcWvUwDqjmCZyg"
+      },
+      {
+        title: "Buceo en Providencia",
+        description: "Explora el tercer arrecife de coral más grande del mundo.",
+        price: "$400 por persona",
+        image: "https://i.ytimg.com/vi/j3rAe7TMMFA/maxresdefault.jpg"
+      },
+      {
+        title: "Sandboard en La Tatacoa",
+        description: "Deslízate por las dunas del desierto de La Tatacoa.",
+        price: "$130 por persona",
+        image: "https://www.mapasybanderas.com/wp-content/uploads/2021/01/sandboard-desierto-de-la-tatacoa.jpg"
+      }
+    ]);
+
+    const currentBanner = computed(() => banners.value[currentBannerIndex.value]);
+
+    const nextSlide = () => {
+      currentBannerIndex.value = (currentBannerIndex.value + 1) % banners.value.length;
     };
-  },
-  computed: {
-    currentBanner() {
-      return this.banners[this.currentBannerIndex];
-    }
-  },
-  mounted() {
-    this.startAutoSlide();
-  },
-  methods: {
-    nextSlide() {
-      this.currentBannerIndex = (this.currentBannerIndex + 1) % this.banners.length;
-    },
-    prevSlide() {
-      this.currentBannerIndex = (this.currentBannerIndex - 1 + this.banners.length) % this.banners.length;
-    },
-    startAutoSlide() {
-      this.autoSlideInterval = setInterval(() => {
-        if (!this.isHovered) {
-          this.nextSlide();
-        }
-      }, 5000);
-    },
-    stopAutoSlide() {
-      clearInterval(this.autoSlideInterval);
-    },
-    onMouseEnter() {
-      this.isHovered = true;
-    },
-    onMouseLeave() {
-      this.isHovered = false;
-    }
-  },
-  beforeDestroy() {
-    this.stopAutoSlide();
+
+    const prevSlide = () => {
+      currentBannerIndex.value = (currentBannerIndex.value - 1 + banners.value.length) % banners.value.length;
+    };
+
+    const startAutoSlide = () => {
+      autoSlideInterval.value = setInterval(nextSlide, 5000);
+    };
+
+    const stopAutoSlide = () => {
+      clearInterval(autoSlideInterval.value);
+    };
+
+    const onMouseEnter = () => {
+      stopAutoSlide();
+    };
+
+    const onMouseLeave = () => {
+      startAutoSlide();
+    };
+
+    onMounted(() => {
+      startAutoSlide();
+    });
+
+    onBeforeUnmount(() => {
+      stopAutoSlide();
+    });
+
+    return {
+      currentBanner,
+      nextSlide,
+      prevSlide,
+      cards,
+      onMouseEnter,
+      onMouseLeave
+    };
   }
 };
 </script>
 
 <style scoped>
+
 .hotel-page {
   background: linear-gradient(to right, #1e88e5, #ffca28);
 }
@@ -180,4 +272,67 @@ export default {
   cursor: pointer;
   margin-top: 5px;
 }
+.footer {
+  background: #333;
+  color: #fff;
+  padding: 20px;
+}
+.footer-content {
+  display: flex;
+  justify-content: space-between;
+}
+.footer {
+  width: 100%; 
+  background-color: #0dc48d;
+  color: #ffffff;
+  padding: 20px 0; 
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+.footer-content {
+  width: 90%;
+  max-width: 1400px; 
+  margin: 0 auto; 
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap; 
+}
+footer.footer{
+  padding: 0;
+  max-width: 150%;
+  margin-top: 20px;
+  margin-left: -20px;
+  margin-bottom: -20px;
+}
+
+.arrow {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  border: none;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  cursor: pointer;
+  z-index: 10;
+}
+
+.arrow-left {
+  left: 15px;
+}
+
+.arrow-right {
+  right: 15px;
+}
+
 </style>
